5c2fac9e4220e0f15de50b7a7f0f0688317add8d
JeanDes-Code <78590821+JeanDes-Code@users.noreply.github.com>
2022-10-07 01:36:06 +0200

    FeaturedPost Carousel

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import {
   PostWidget,
 } from '../components';
 import { getPosts } from '../services';
+import { FeaturedPosts } from './../sections';
 
 export default function Home({ posts }) {
   return (
@@ -14,6 +15,7 @@ export default function Home({ posts }) {
         <title>Blog - Jean Desauw </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post) => (
